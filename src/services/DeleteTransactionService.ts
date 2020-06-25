eb33db0a51e218bf52818c2b8d8377839391a446
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-25 15:49:33 -0300

    Create DeleteTransaction service

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,17 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getRepository('transactions');
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction not found.', 404);
+    }
+
+    await transactionsRepository.delete(id);
   }
 }
 
