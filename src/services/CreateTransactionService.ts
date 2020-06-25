dc98f8490ea6cdc9ea8440de7cbca21e0980ce9b
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-25 15:49:22 -0300

    Create CreateTransaction service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,63 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+export interface Request {
+  title: string;
+  value: number;
+  type: string;
+  categoryTitle: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    categoryTitle,
+  }: Request): Promise<Transaction> {
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Invalid type.');
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+
+      if (value >= total) {
+        throw new AppError('Insuficient balance', 400);
+      }
+    }
+
+    const categoriesRepository = getRepository(Category);
+    let category = await categoriesRepository.findOne({
+      where: { title: categoryTitle },
+    });
+
+    if (!category) {
+      category = categoriesRepository.create({
+        title: categoryTitle,
+      });
+
+      await categoriesRepository.save(category);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
